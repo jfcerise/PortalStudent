65d1ef3ba8a9a086bd67c0ef2734f739b99e674d
ben022 <benjamin.pieron@gmail.com>
2019-06-19 16:55:41 +0200

    Commit avec lien dans View Edit Sandwsh (javascript) qui ne fonctionne pas encore

---
+++ PortalStudent.UseCases/AdminRole/AdminRole - EditSandwichInMenu.cs
@@ -0,0 +1,33 @@
+﻿using PortalStudent.Common.Domain;
+using PortalStudent.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalStudent.UseCases
+{
+    public partial class AdminRole
+    {
+        public bool EditSandwichInMenu(Sandwich sandwichToEdit)
+        {
+            if (sandwichToEdit == null)
+                throw new ArgumentNullException(nameof(sandwichToEdit));
+
+            if (sandwichToEdit.SandwichId == 0)
+                throw new Exception(nameof(sandwichToEdit));
+
+            if (String.IsNullOrEmpty(sandwichToEdit.Name) || String.IsNullOrWhiteSpace(sandwichToEdit.Name))
+                throw new Exception(nameof(sandwichToEdit));
+
+            using (var ctx = new PortalContext())
+            {
+                ctx.Entry(sandwichToEdit).State = EntityState.Modified;
+                ctx.SaveChanges();
+            }
+            return true;
+        }
+    }
+}
